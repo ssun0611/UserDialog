e872249867f95a25d6ce7843e944ebdee57effa1
ssun0611 <rlathfdl463@naver.com>
2017-05-30 19:27:23 +0900

    #3 Event Programing2

--- app/src/main/java/rlathfdl463/kr/hs/emirim/userdialog/MainActivity.java
+++ app/src/main/java/rlathfdl463/kr/hs/emirim/userdialog/MainActivity.java
@@ -1,12 +1,19 @@
 package rlathfdl463.kr.hs.emirim.userdialog;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView textName, textEmail;
+    EditText editName, editEmail;
+    View dlgV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,16 +21,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button but=(Button) findViewById(R.id.but);
         but.setOnClickListener(this);
+        textName=(TextView)findViewById(R.id.text_name);
+        textEmail=(TextView)findViewById(R.id.text_email);
     }
 
     @Override
-    public void onClick(View v) {
-        View dlgV=View.inflate(this,R.layout.dialog,null);
+    public void onClick(View view) {
+        final View dlgV=View.inflate(this,R.layout.dialog,null);
         AlertDialog.Builder dialog=new AlertDialog.Builder(this);
         dialog.setTitle("Input User Information");
         dialog.setIcon(R.drawable.tangguri);
-        dialog.setPositiveButton("OK",null);
-        dialog.setNegativeButton("NO",null);
+        dialog.setView(dlgV);
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                editName=(EditText) dlgV.findViewById(R.id.edit_name);
+                editEmail=(EditText) dlgV.findViewById(R.id.edit_email);
+                textName.setText(editName.getText());
+                textEmail.setText(editEmail.getText());
+            }
+        });
+        dialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast toast=new Toast(MainActivity.this);
+                View toastV=View.inflate(MainActivity.this,R.layout.toast,null);
+                TextView textToast=(TextView)toastV.findViewById(R.id.text_toast);
+                textToast.setText("철컹철컹");
+                toast.setView(toastV);
+                toast.show();
+            }
+        });
         dialog.setView(dlgV);
 
         dialog.show();
